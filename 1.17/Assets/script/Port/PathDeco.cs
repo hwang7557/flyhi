acabccda4135f3d0c28db9c497123142c2562a32
jyhwang <hwang7557@naver.com>
2017-02-21 12:08:32 +0900

    f

---
+++ 1.17/Assets/script/Port/PathDeco.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDeco : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PathTilingScaleChange(float TileSIzeX, float TileSizeY)
+    {
+        Material ResourcesTemp = Resources.Load("Prefebs/Ground10", typeof(Material)) as Material;
+        Material ChangeMaterial = Instantiate(ResourcesTemp) as Material;
+        ChangeMaterial.mainTextureScale = new Vector2(TileSIzeX * 0.2f, TileSizeY * 0.2f);
+        gameObject.GetComponent<Renderer>().material = ChangeMaterial;
+    }
+}
---
+++ 1.17/Assets/script/Port/RoomDeco.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDeco : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void floorTilingScaleChange(float TileSIzeX, float TileSizeY)
+    {
+        Material ResourcesTemp = Resources.Load("Prefebs/Ground04", typeof(Material)) as Material;
+        Material ChangeMaterial = Instantiate(ResourcesTemp) as Material;
+        ChangeMaterial.mainTextureScale = new Vector2(TileSIzeX * 0.2f, TileSizeY * 0.2f);
+        gameObject.GetComponent<Renderer>().material = ChangeMaterial;
+    }
+}
